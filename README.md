55c3229056fe4e75a2fd76925e76f791f701bbe6
Fabiano <fabinppk@hotmail.com>
2020-12-02 15:33:52 -0300

    feat: initial description

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# back-end-challenge
+# Back-end Challenge
+
+## Notas gerais
+
+- Não compartilhe seu teste publicamente em nenhuma plataforma durante o processo de seleção.
+- Recomendamos que use um reposítório privado no Github e de acesso aos seguintes usuários: `fabinppk`, `cheldernunes`, `mauriciors`, `silveiradasilvaa` e `rafaelnogueira1`. (Em útimo caso, se tiver problemas em adicionar usuários em um repositório privado, deixe-o público e nos mande o link dele.)
+
+## Nossas expectativas
+
+Esperamos que seu trabalho reflita qualidade, extensividade e manutenibilidade, por isso sinta-se à vontade para mostrar isso ao desenvolver sua solução.
+
+## Objetivo
+
+Criar uma aplicação usando python/django que atenda os requisitos a seguir.
+
+#### Tenha os seguintes end points:
+
+- Listagem de todas as vitrines disponíveis
+- Listagem de vitrines baseado em rotas
+
+## Exemplos e/ou sugestões
+
+#### Listagem de todas as vitrines
+
+- URL: `/vitrines/`
+- Retorno: [ver arquivo](files/all.js)
+
+#### Listagem das vitrines para página `/`
+
+- URL: `/vitrines/`
+- Payload: {routes: ["/"]}
+- Retorno: [ver arquivo](files/home.js)
+
+#### Listagem das vitrines para página `/destinos`
+
+- URL: `/vitrines/`
+- Payload: {routes: ["/destinos"]}
+- Retorno: [ver arquivo](files/destinos.js)
+
+#### Listagem das vitrines para página `/sobre`
+
+- URL: `/vitrines/`
+- Payload: {routes: ["/sobre"]}
+- Retorno: [ver arquivo](files/sobre.js)
+
+## Informações adicionais
+
+- Fique a vontade para criar mais dados para enriquecer o retorno da sua API, mas lembre-se, eles devem mater o formato solicitado.
+- Você pode usar mocks baseado em arquivos, porém o uso de um banco de dados com certeza te colocará na frente.
+- Foi solicitado apenas um tipo de "filtro" nos end points (o de `routes`), mas fique a vontade para criar mais além desse, por exemplo o de `limit` que limita a quantidade de `itens` dentro do retorno.
---
+++ files/all.js
@@ -0,0 +1,308 @@
+export const vitrine = [
+  {
+    id: 1,
+    title: "Melhores Hotéis",
+    subtitle: "Veja os hotéis mais procurados",
+    routes: ["/", "/destinos"],
+    itens: [
+      {
+        hotel_name: "Alameda Alegra",
+        slug: "alameda-alegra",
+        image: "http://sbtur.com/images/alameda-alegra.jpg",
+        city: {
+          name: "Gramado",
+          slug: "gramado",
+          state: "RS",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 3500,
+      },
+      {
+        hotel_name: "AJ Termas Iraí",
+        slug: "aj-termas-irai",
+        image: "http://sbtur.com/images/aj-termas-irai.jpg",
+        city: {
+          name: "Iraí",
+          slug: "irai",
+          state: "RS",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 6900,
+      },
+      {
+        hotel_name: "Allia Gran Hotel Brasília Suites/ Allia Hotel",
+        slug: "allia-gran-hotel-brasilia-suites",
+        image: "http://sbtur.com/images/allia-gran-hotel-brasilia-suites.jpg",
+        city: {
+          name: "Brasília",
+          slug: "brasilia",
+          state: "DF",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Apartamento",
+          slug: "apartamento",
+        },
+        price: 4950,
+      },
+      {
+        hotel_name: "Hotel Fazenda Dona Francisca",
+        slug: "hotel-fazenda-dona-francisca",
+        image: "http://sbtur.com/images/hotel-fazenda-dona-francisca.jpg",
+        city: {
+          name: "Joinville",
+          slug: "joinville",
+          state: "SC",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hostel",
+          slug: "hostel",
+        },
+        price: 4950,
+      },
+      {
+        hotel_name: "Slavieiro Florianópolis Baia Norte",
+        slug: "slavieiro-florianopolis-baia-norte",
+        image: "http://sbtur.com/images/slavieiro-florianopolis.jpg",
+        city: {
+          name: "Florianópolis",
+          slug: "florianopolis",
+          state: "SC",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 5230,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Pacotes imperdíveis",
+    subtitle: "Procurando pacotes em família?",
+    routes: ["/"],
+    itens: [
+      {
+        hotel_name: "Slavieiro Florianópolis Baia Norte",
+        slug: "slavieiro-florianopolis-baia-norte",
+        image: "http://sbtur.com/images/slavieiro-florianopolis.jpg",
+        city: {
+          name: "Florianópolis",
+          slug: "florianopolis",
+          state: "SC",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 5230,
+      },
+      {
+        hotel_name: "Allia Gran Hotel Brasília Suites/ Allia Hotel",
+        slug: "allia-gran-hotel-brasilia-suites",
+        image: "http://sbtur.com/images/allia-gran-hotel-brasilia-suites.jpg",
+        city: {
+          name: "Brasília",
+          slug: "brasilia",
+          state: "DF",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Apartamento",
+          slug: "apartamento",
+        },
+        price: 4950,
+      },
+      {
+        hotel_name: "Hotel Fazenda Dona Francisca",
+        slug: "hotel-fazenda-dona-francisca",
+        image: "http://sbtur.com/images/hotel-fazenda-dona-francisca.jpg",
+        city: {
+          name: "Joinville",
+          slug: "joinville",
+          state: "SC",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hostel",
+          slug: "hostel",
+        },
+        price: 4950,
+      },
+      {
+        hotel_name: "Intercity Cidade Baixa",
+        slug: "intercity-cidade-baixa",
+        image: "http://sbtur.com/images/intercity-cidade-baixa.jpg",
+        city: {
+          name: "Porto Alegre",
+          slug: "porto-alegre",
+          state: "RS",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 8300,
+      },
+      {
+        hotel_name: "Ibis Poços de Caldas",
+        slug: "ibis-pocos-de-caldas",
+        image: "http://sbtur.com/images/ibis-pocos-de-caldas.jpg",
+        city: {
+          name: "Poços de Caldas",
+          slug: "pocos-de-caldas",
+          state: "MG",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 6250,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Hotéis na região sul",
+    subtitle: "Veja os melhores hotéis do RS",
+    routes: [],
+    itens: [
+      {
+        hotel_name: "Alameda Alegra",
+        slug: "alameda-alegra",
+        image: "http://sbtur.com/images/alameda-alegra.jpg",
+        city: {
+          name: "Gramado",
+          slug: "gramado",
+          state: "RS",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 3500,
+      },
+      {
+        hotel_name: "Intercity Cidade Baixa",
+        slug: "intercity-cidade-baixa",
+        image: "http://sbtur.com/images/intercity-cidade-baixa.jpg",
+        city: {
+          name: "Porto Alegre",
+          slug: "porto-alegre",
+          state: "RS",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 8300,
+      },
+      {
+        hotel_name: "Ibis Poços de Caldas",
+        slug: "ibis-pocos-de-caldas",
+        image: "http://sbtur.com/images/ibis-pocos-de-caldas.jpg",
+        city: {
+          name: "Poços de Caldas",
+          slug: "pocos-de-caldas",
+          state: "MG",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 6250,
+      },
+      {
+        hotel_name: "AJ Termas Iraí",
+        slug: "aj-termas-irai",
+        image: "http://sbtur.com/images/aj-termas-irai.jpg",
+        city: {
+          name: "Iraí",
+          slug: "irai",
+          state: "RS",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        category: {
+          name: "Hospedagem",
+          slug: "hospedagem",
+        },
+        price: 6900,
+      },
+      {
+        hotel_name: "Slavieiro Florianópolis Baia Norte",
+        slug: "slavieiro-florianopolis-baia-norte",
+        image: "http://sbtur.com/images/slavieiro-florianopolis.jpg",
+        city: {
+          name: "Florianópolis",
+          slug: "florianopolis",
+          state: "SC",
+        },
+        country: {
+          name: "Brasil",
+          slug: "brasil",
+        },
+        type: "Hospedagem",
+        price: 5230,
+      },
+    ],
+  },
+];
